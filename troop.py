93a047a3496ea57148c863713d85325334ab65c2
xymin <xyminimal@outlook.com>
2022-10-06 23:40:06 +0100

    Added some blank classes

---
+++ troop.py
@@ -0,0 +1,7 @@
+class Troop:
+    def __init__(self):
+        pass
+
+    def verifyMove(self, move):
+        pass
+
---
+++ troop_list.py
@@ -0,0 +1,39 @@
+from troop import Troop
+
+
+class Pawn(Troop):
+    def __init__(self):
+        super(Pawn, self).__init__()
+        pass
+
+
+class Rook(Troop):
+    def __init__(self):
+        super(Rook, self).__init__()
+        pass
+
+
+class Bishop(Troop):
+    def __init__(self):
+        super(Bishop, self).__init__()
+        pass
+
+
+class Knight(Troop):
+    def __init__(self):
+        super(Knight, self).__init__()
+        pass
+
+
+class King(Troop):
+    def __init__(self):
+        super(King, self).__init__()
+        pass
+
+
+class Queen(Troop):
+    def __init__(self):
+        super(Queen, self).__init__()
+        pass
+
+
