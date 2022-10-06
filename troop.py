b59c2f111233757ed46386f7074c3ff5205162dc
xymin <xyminimal@outlook.com>
2022-10-07 00:28:20 +0100

    Added some broken functions and inheritance, will fix saturday

--- troop.py
+++ troop.py
@@ -1,7 +1,21 @@
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QLabel
+
+
 class Troop:
-    def __init__(self):
-        pass
+    def __init__(self, window, team):
+        self.frame = None
+        self.window = window
+        self.image = None
+        self.icon = None
+        self.team = team
 
     def verifyMove(self, move):
         pass
 
+    def draw(self):
+        self.image = QPixmap(f"assets\\{self.team}\\{self.icon}")
+        self.frame = QLabel(self.window)
+        self.frame.setPixmap(self.image)
+        self.frame.resize(self.image.width(),
+                          self.image.height())
--- troop_list.py
+++ troop_list.py
@@ -1,39 +1,40 @@
 from troop import Troop
-
+from PyQt6.QtGui import QPixmap
+from PyQt6.QtWidgets import QLabel
 
 class Pawn(Troop):
-    def __init__(self):
-        super(Pawn, self).__init__()
+    def __init__(self, window, team):
+        super(Pawn, self).__init__(window, team)
         pass
 
 
 class Rook(Troop):
-    def __init__(self):
-        super(Rook, self).__init__()
+    def __init__(self, window, team):
+        super(Rook, self).__init__(window, team)
         pass
 
 
 class Bishop(Troop):
-    def __init__(self):
-        super(Bishop, self).__init__()
+    def __init__(self, window, team):
+        super(Bishop, self).__init__(window, team)
         pass
 
 
 class Knight(Troop):
-    def __init__(self):
-        super(Knight, self).__init__()
+    def __init__(self, window, team):
+        super(Knight, self).__init__(window, team)
         pass
 
 
 class King(Troop):
-    def __init__(self):
-        super(King, self).__init__()
+    def __init__(self, window, team):
+        super(King, self).__init__(window, team)
         pass
 
 
 class Queen(Troop):
-    def __init__(self):
-        super(Queen, self).__init__()
+    def __init__(self, window, team):
+        super(Queen, self).__init__(window, team)
         pass
 
 
